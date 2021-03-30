5a108289b7753d50930d548daf759bff33a34d70
Matoba Taiki <tomatomatobanana@gmail.com>
2021-03-30 23:20:11 +0900

    ユーザー登録ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録完了しました"
+      redirect_to '/'
+    else
+      render 'new'	
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'top_page#home'
   get :about,  to: 'top_page#about'
   get :signup, to: 'users#new'
+  resources :users
 end
